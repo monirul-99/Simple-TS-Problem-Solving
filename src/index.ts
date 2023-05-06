69c7116bb10227d7a5816ce2b8d916787301a27b
monaulic <sohelmuhammad164@gmail.com>
2023-05-06 11:11:29 +0600

    JS Array Convert To TS Tuple

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+// 1 Convert the following JavaScript array into a TypeScript tuple
+// const userInfo = [101, "Ema", "John", true,  , "2023"];
+let myTuple : [number, string, string, boolean, any, string]
+myTuple=[101, "Ema", "John", true,  , "2023"]
+// console.log(myTuple);
+
